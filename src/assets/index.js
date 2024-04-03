2dd72a29ec0e299aebf5510d76bd50caf4638f1f
Robin Sen <robinsen@Robins-MacBook-Air.local>
2024-04-03 23:19:13 +0530

    Sidebar editing

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,21 +1,27 @@
-import dashboardIcon from "./icons/home.png";
-import setParametersIcon from "./icons/settings.png";
-import logoutIcon from "./icons/logout.png";
-import databaseIcon from "./icons/database.png";
-import troubleshootIcon from "./icons/crisis_alert.png";
-import supportDeskIcon from "./icons/support_agent.png";
-import instructionsIcon from "./icons/fiber_manual_record.png";
-import logo from "./icons/Logo copy 3.png";
-import menuIcon from "./icons/menu_24px.png"
+import { IoMdSearch } from "react-icons/io";
+import { IoStopOutline } from "react-icons/io5";
+import { MdSpeed } from "react-icons/md";
+import { BsFastForward } from "react-icons/bs";
+import { BsRewind } from "react-icons/bs";
+import { IoAppsOutline } from "react-icons/io5";
+import { LuTimerReset } from "react-icons/lu";
+import { PiBroom } from "react-icons/pi";
+import { RxHamburgerMenu } from "react-icons/rx";
+
+import logo from "./icons/Logo.svg";
+import logoName from "./icons/logo name.svg";
 
 export {
-  dashboardIcon,
-  setParametersIcon,
-  logoutIcon,
-  databaseIcon,
-  troubleshootIcon,
-  supportDeskIcon,
-  instructionsIcon,
+  IoMdSearch,
+  IoStopOutline,
+  MdSpeed,
+  BsFastForward,
+  BsRewind,
+  IoAppsOutline,
+  LuTimerReset,
+  PiBroom,
+  RxHamburgerMenu,
+  
+  logoName,
   logo,
-  menuIcon
 };
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,11 @@
 import { Link, useLocation } from "react-router-dom";
 import React, { useEffect, useState } from "react";
-import { logo, menuIcon } from "../assets";
+import { RxHamburgerMenu,logo,logoName  } from "../assets";
 import { navLinks } from "../constants";
 import { useNavigate } from "react-router-dom";
 
-function Sidebar({ open, setOpen, navBg, navHeading }) {
+
+function Sidebar({ open, setOpen, navBg }) {
   // const location = useLocation();
   // const navigate = useNavigate();
 
@@ -13,43 +14,40 @@ function Sidebar({ open, setOpen, navBg, navHeading }) {
       <nav id="nav" className="top-0 fixed shadow-md shadow-slate-400 z-10">
         <div
           style={{ backgroundColor: `var(--${navBg})` }}
-          className={`flex flex-col  justify-between pb-20 h-screen ${
+          className={`flex flex-col  justify-between pb-14 h-screen ${
             open ? "w-[210px]" : "w-[64px]"
           } transition-all duration-500 delay-[25ms] text-gray-100 px-4`}
         >
           <div>
             <div
-              className={`py-3 pl-1 flex flex-nowrap overflow-hidden items-center `}
+              className={`py-3 flex flex-nowrap  items-center `}
             >
-              <img
-                src={menuIcon}
-                size={24}
-                className="cursor-pointer"
-                onClick={() => setOpen(!open)}
-              />
+             
+              <div className=" text-nowrap">
+               <img
+                  src={logo}
+                 
+                  alt="Logo"
+                  className="inline w-10  mx-auto"
+                />
+                <img
+                 src={logoName}
+                  // style={{
+                  //   transitionDelay: `${2}00ms`,
+
+                  // }}
+                  className={`inline ml-3 transition-all duration-300 ${
+                    !open && "opacity-0 translate-x-24"
+                  }`}
+                />
+              </div>
             </div>
             <div className="mt-4 flex flex-col gap-4 justify-between relative">
-              <div className="overflow-hidden text-nowrap">
-                <img
-                  src={logo}
-                  width={30}
-                  height={30}
-                  alt="Logo"
-                  className="inline"
-                />
-                <h2
-                  style={{
-                    transitionDelay: `${2}00ms`,
-                    // color: `var(--${navHeading})`,
-                  }}
-                  className={`whitespace-pre duration-300 uppercase leading-none relative -bottom-1 text-black font-semibold text-2xl  inline ${
-                    !open && "opacity-0 translate-x-28 overflow-hidden"
-                  }`}
-                >
-                  Eythor
-                </h2>
-              </div>
-
+            <RxHamburgerMenu
+                size={26}
+                className="cursor-pointer text-black "
+                onClick={() => setOpen(!open)}
+              />
               {navLinks.slice(0,4).map((link, i) => (
               <Link
                 to={link.link}
@@ -60,7 +58,7 @@ function Sidebar({ open, setOpen, navBg, navHeading }) {
                   open ? " pl-1 " : "pl-0"
                 } transition-[padding] duration-500 text-black hover:text-sky-500 rounded-md`}
               >
-                <div>{React.createElement(link.icon, { size: "30" })}</div>
+                <div>{React.createElement(link.icon, { size: "26" })}</div>
                 <h2
                   className={`whitespace-pre text-md duration-300 ${
                     !open && "opacity-0 translate-x-24 overflow-hidden"
@@ -89,11 +87,11 @@ function Sidebar({ open, setOpen, navBg, navHeading }) {
              key={i}
              className={` ${
                link.margin && "mt-5"
-             } group flex items-center text-sm py-3 gap-3.5 font-medium py-1  ${
+             } group flex items-center text-sm py-3 gap-3.5 font-medium   ${
                open ? " pl-1 " : "pl-0"
              } transition-[padding] duration-500 text-black hover:text-sky-500 rounded-md`}
            >
-             <div>{React.createElement(link.icon, { size: "30" })}</div>
+             <div>{React.createElement(link.icon, { size: "26" })}</div>
              <h2
                className={`whitespace-pre text-md duration-300 ${
                  !open && "opacity-0 translate-x-24 overflow-hidden"
