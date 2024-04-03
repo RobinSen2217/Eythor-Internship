39c715a79580d9d1acd162774fc82edf7a777511
Robin Sen <robinsen@Robins-MacBook-Air.local>
2024-04-04 00:54:17 +0530

    added all icons

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,6 +7,12 @@ import { IoAppsOutline } from "react-icons/io5";
 import { LuTimerReset } from "react-icons/lu";
 import { PiBroom } from "react-icons/pi";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { IoIosWarning } from "react-icons/io";
+import { CgProfile } from "react-icons/cg";
+import { MdOutlineZoomIn } from "react-icons/md";
+import { MdOutlineZoomOut } from "react-icons/md";
+import { MdOutlineLogout } from "react-icons/md";
+import { IoLanguageOutline } from "react-icons/io5";
 
 import logo from "./icons/Logo.svg";
 import logoName from "./icons/logo name.svg";
@@ -21,6 +27,12 @@ export {
   LuTimerReset,
   PiBroom,
   RxHamburgerMenu,
+  IoIosWarning,
+  CgProfile,
+  MdOutlineZoomIn,
+  MdOutlineZoomOut,
+  MdOutlineLogout,
+  IoLanguageOutline,
   
   logoName,
   logo,
